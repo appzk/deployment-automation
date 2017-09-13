990653852697842bbb678b7eadc612916feedd62
Henry Li <henry1943@163.com>
2017-09-13 22:20:30 +0800

    Add Version

---
+++ src/cpa/components/Version/index.jsx
@@ -0,0 +1,11 @@
+import React, { PureComponent } from 'react';
+
+export default class Version extends PureComponent {
+  render() {
+    return (
+      <div>
+        Version: 1.0
+      </div>
+    );
+  }
+}
