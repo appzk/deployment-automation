1217f25648eb1b0942faabf774ed578a42d06246
Henry Li <henry1943@163.com>
2017-09-14 20:54:10 +0800

    Update publish.sh

---
+++ publish.sh
@@ -0,0 +1,14 @@
+function parse_git_dirty {
+  [[ $(git status 2> /dev/null | tail -n1) != "nothing to commit (working directory clean)" ]] && echo "*"
+}
+
+function parse_git_branch {
+  git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/(\1$(parse_git_dirty))/"
+}
+
+BRANCH=$(parse_git_branch)
+if [ $BRANCH == "(development*)" ]; then
+  echo "请先完成本地提交，再重新尝试"
+elif [ $BRANCH != "(development)" ]; then
+  echo "当前不是 development 分支，请先执行 git checkout development"
+fi
