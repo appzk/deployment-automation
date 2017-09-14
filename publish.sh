ce96445b997b4e332d3462f5ef8a6b39730d8cee
Henry Li <henry1943@163.com>
2017-09-14 21:10:01 +0800

    Update publish.sh

--- publish.sh
+++ publish.sh
@@ -11,4 +11,17 @@ if [ $BRANCH == "(development*)" ]; then
   echo "$BRANCH 尚未完成本地提交。"
 elif [ $BRANCH != "(development)" ]; then
   echo "当前 $BRANCH 不是 development 分支，请先执行 git checkout development 切换分支"
+else
+  git push origin development
+  git checkout master
+  git merege development --no-ff --quiet
+  git push origin master
+  git checkout development
+  echo "已完成 master 分支合并，并已触发云端打包发布流程"
+  echo
+  echo "请在 Travis Dashboard 中查看结果"
+  echo "https://www.travis-ci.org/MagicCube/cdn-pub-automation/"
+  echo
+  echo "Travis 可能会应高峰而延迟打包，你也可以在这里查看最新结果"
+  echo "https://www.travis-ci.org/MagicCube/cdn-pub-automation/branches"
 fi
