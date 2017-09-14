59f191d78a8cacd73da6ca2c3969fe59ff6a9063
Henry Li <henry1943@163.com>
2017-09-14 20:25:09 +0800

    Remove Version

--- src/cpa/components/App/index.jsx
+++ src/cpa/components/App/index.jsx
@@ -1,7 +1,5 @@
 import React, { PureComponent } from 'react';
 
-import Version from '../Version';
-
 import styles from './index.less';
 
 export default class App extends PureComponent {
@@ -9,7 +7,6 @@ export default class App extends PureComponent {
     return (
       <div className={styles.container}>
         <h1>Welcome to cdn-pub-automation</h1>
-        <Version />
         <div>
           For more details, please visit&nbsp;
           <a href="https://zhuanlan.zhihu.com/p/29231319/edit">https://zhuanlan.zhihu.com/p/29231319/edit</a>
